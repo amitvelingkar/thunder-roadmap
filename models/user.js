165dbfc3f314df4ffb7d58307df73d2582c4626c
Amit Velingkar <amit.velingkar@paperg.com>
2017-05-26 06:33:35 -0700

    Bug Fixes

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
-const mongoose = reqire('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('password-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     email: {
